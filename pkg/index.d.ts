14278be1a225d5d37034a9637eddbc8058d73fb0
Russell Snyder <russellsnyder@Russells-MBP-2.fritz.box>
2021-01-22 01:43:37 +0100

    add pkg for netlify

---
+++ pkg/index.d.ts
@@ -0,0 +1,15 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+*/
+export function main_js(): void;
+/**
+* @param {string} noise_type
+* @returns {Handle}
+*/
+export function play_noise(noise_type: string): Handle;
+/**
+*/
+export class Handle {
+  free(): void;
+}
